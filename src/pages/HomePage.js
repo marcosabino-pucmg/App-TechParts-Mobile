b26d53e9122920098a2c2e8451162dc64c63555d
Marcos S. M. Sousa <112666835+marcosabino-pucmg@users.noreply.github.com>
2025-05-29 15:01:50 +0000

    import app tech parts

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const HomePage = () => {
+
+  return (
+
+    <View>
+    
+      <Text> cadelona veia </Text>
+
+    </View>
+
+  );
+
+}
+
+export default HomePage;
